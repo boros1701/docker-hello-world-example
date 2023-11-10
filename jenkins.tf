5c4bda2134872bb33157f047dbdf7559cb32a6c0
boros1701 <97278953+boros1701@users.noreply.github.com>
2023-11-10 13:33:57 +0200

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -2,7 +2,7 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://charts.jenkins.io"
   chart      = "jenkins"
-  version    = "3.6.0"
+  version    = "3.12.1"
   namespace  = "jenkins"
   timeout    = 600
   values = [
