491e5b456e37653782203132cd424546947506c5
boros1701 <97278953+boros1701@users.noreply.github.com>
2023-11-12 18:22:39 +0200

    Update config.groovy

--- config.groovy
+++ config.groovy
@@ -2,7 +2,7 @@ job('Gradle build for Java app') {
     scm {
         git('https://github.com/boros1701/luminor_task.git'){
            node / gitConfigName('DSL User')
-           node / gitConfigEmail('boros2y6@inbxo.lv')
+           node / gitConfigEmail('boros2y6@inbox.lv')
     }
     triggers {
         scm ('H/5 * * * *')
