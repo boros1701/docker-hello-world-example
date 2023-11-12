638dd52a0c0d93f45c757fa017f68024e3fd509a
boros1701 <97278953+boros1701@users.noreply.github.com>
2023-11-12 18:15:05 +0200

    Create config.groovy

---
+++ config.groovy
@@ -0,0 +1,13 @@
+job('Gradle build for Java app') {
+    scm {
+        git('https://github.com/boros1701/luminor_task.git'){
+           node / gitConfigName('DSL User')
+           node / gitConfigEmail('boros2y6@inbxo.lv')
+    }
+    triggers {
+        scm ('H/5 * * * *')
+    }
+    steps {
+        gradle 'assemble'
+    }
+}
